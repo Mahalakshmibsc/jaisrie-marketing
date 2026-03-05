bd15b1beb89408da18dccf79977eb6f4f2da2a8f
Mahalakshmi <136691991+Mahalakshmibsc@users.noreply.github.com>
2026-03-05 18:35:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-document.getElementById("contactForm")?.addEventListener("submit", function(e){
-    e.preventDefault();
-    alert("Thank you! We have received your message.");
-});
+let testimonials = document.querySelectorAll(".testimonial");
+
+let index = 0;
+
+function showTestimonials(){
+
+testimonials.forEach(t => t.classList.remove("active"));
+
+index++;
+
+if(index > testimonials.length){
+index = 1;
+}
+
+testimonials[index-1].classList.add("active");
+
+}
+
+setInterval(showTestimonials,4000);
